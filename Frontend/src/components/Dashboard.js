6c0a43ac52e84b8bbf021dc7282e8e9df01a9a50
Wardatuljanna <89592051+Wardatuljanna@users.noreply.github.com>
2023-12-08 17:37:53 +0800

    update Dashboard.js

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
   const [inventoryData, setInventoryData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://backend-inventory-project-production.up.railway.app/products')
+    axios.get('https://backend-inventory-project-production.up.railway.app/products')
       .then(response => {
         console.log('Data from API:', response.data);
         setInventoryData(response.data);
